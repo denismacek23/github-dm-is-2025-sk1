27efdb9f1e71ab1fa9d247d82d5b8ce5037ef109
denismacek23 <denis.macek@tul.cz>
2025-10-01 10:24:12 +0200

    #3 Základ programu pro výpis řady

---
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,29 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("******************");
+    Console.WriteLine("**** Výpis řady čísel ****");
+    Console.WriteLine("******************");
+    Console.WriteLine("**** Denis Macek ****");
+    Console.WriteLine("******************");
+    Console.WriteLine();
+
+    // vstup hodnoty do programu - špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    Console.Write("Zadejte první číslo řady(celé číslo): ");
+    int first;
+
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady: ");
+    }
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu 'a'.");
+    again = Console.ReadLine();
+}
