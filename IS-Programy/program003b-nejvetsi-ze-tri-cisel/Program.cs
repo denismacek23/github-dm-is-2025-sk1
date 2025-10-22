200390a2652148cfeaacefd6bcc9f9e998cd3d29
denismacek23 <denis.macek@tul.cz>
2025-10-22 10:02:04 +0200

    #6 Nejvetsi ze tri cisel

---
+++ IS-Programy/program003b-nejvetsi-ze-tri-cisel/Program.cs
@@ -0,0 +1,62 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("****************************");
+    Console.WriteLine("***** Nejvetsi ze tri cisel *****");
+    Console.WriteLine("****************************");
+    Console.WriteLine("******* Denis Macek ********");
+    Console.WriteLine("****************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu - špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    //Vstup hodnoty do programu - řešený správně
+    Console.Write("Zadejte hodnotu prvního čísla A: ");
+    int a;
+
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu A znovu: ");
+    }
+
+    Console.Write("Zadejte hodnotu prvního čísla B: ");
+    int b;
+
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu B znovu: ");
+    }
+
+Console.Write("Zadejte hodnotu prvního čísla C: ");
+    int c;
+
+    while (!int.TryParse(Console.ReadLine(), out c))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu C znovu: ");
+    }
+
+    Console.WriteLine("*********************************************");
+    if (a > b) {
+        if (a > c) 
+            Console.WriteLine($"Největší číslo je A = {a}");
+        else
+            Console.WriteLine($"Největší číslo je C = {c}"); 
+    } else {
+        if (b > c)
+            Console.WriteLine($"Největší číslo je B = {b}");
+        else 
+            Console.WriteLine($"Největší číslo je C = {c}");
+    } 
+    Console.WriteLine("*********************************************");
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+
+
+}
